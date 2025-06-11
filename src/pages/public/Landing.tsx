1897c25133f2b67425b513d6f2c8b79e063a5e1d
Cam Johnson <cameron.n.johnson15@gmail.com>
2025-06-11 12:24:24 -0600

    Add landing page

---
+++ src/pages/public/Landing.tsx
@@ -0,0 +1,17 @@
+import CTA from './CallToAction';
+import Features from './Features';
+import Footer from './Footer';
+import Hero from './Hero';
+
+const Landing = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <CTA />
+      <Footer />
+    </>
+  );
+};
+
+export default Landing;
