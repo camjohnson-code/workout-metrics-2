173a61ac0b7120a447f1c575ff32b8e438a68a94
Cam Johnson <cameron.n.johnson15@gmail.com>
2025-06-11 14:58:46 -0600

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/public/Landing';
+import Login from './pages/auth/Login';
+import Callback from './pages/auth/strava/Callback';
 
 function App() {
-  return <Landing />;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/auth/strava/callback' element={<Callback />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
